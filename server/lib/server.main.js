a13e897bd3ac7424c4d94bb736fcf74f8aab674e
QuaintShanty <quaint.shanty@gmail.com>
2019-10-28 12:36:46 -0400

    Updated the build script.

--- server/lib/server.main.js
+++ server/lib/server.main.js
@@ -1,2 +1,2 @@
 import 'dotenv.config'; // Starts the server off with dotenv loading process.env variables.
-import 'express.main'; // Run the express server
+import './express/express.main'; // Run the express server
